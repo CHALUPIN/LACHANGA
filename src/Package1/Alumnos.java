1c635c08f6935f3abaf9c31f4c5851b3bbda8dfe
Kike <angrylocobirds@gmail.com>
2021-02-17 20:54:03 -0600

    Cambios por kike

--- src/Package1/Alumnos.java
+++ src/Package1/Alumnos.java
@@ -2,16 +2,8 @@ package Package1;
 
 import java.util.*;
 
-
-//cambios para clonar correctamente
 //cambios hechos por kike
-//cambios echos por abraham
-// cambios para que el Luis me la mame
-//cambios  echos por faby :D
-//funciona porfavor by:arath
-//cmabios echos pro :Luis A
 
-//cambios echos por abraham
 
 /**
  * 
